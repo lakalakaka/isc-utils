07d954861d93787ab05a0812c63b8c03dd240c5b
肖波 <xiaob@isyscore.com>
2020-04-19 20:14:15 +0800

    更新

--- packages/isyscore-utils.typeis/lib/index.ts
+++ packages/isyscore-utils.typeis/lib/index.ts
@@ -4,12 +4,12 @@
  * @Author: xiaobo
  * @Date: 2020-04-16 16:17:28
  * @LastEditors: xiaobo
- * @LastEditTime: 2020-04-19 20:02:58
+ * @LastEditTime: 2020-04-19 20:13:59
  */
 'use strict';
 
 
 export function isyscoreUtilsTypeis() {
-    console.log('isyscoreUtilsTypeis/test')
+    console.log('isyscoreUtilsTypeis')
     // TODO
 }
